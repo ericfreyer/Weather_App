f76664b7ad25309a3bf3d80dee1649cd578a70d5
Eric Freyer <ericcfreyer@gmail.com>
2020-12-13 21:57:21 -0500

    added rest of weather specs

--- script.js
+++ script.js
@@ -4,24 +4,38 @@ $("#Search-Btn").on("click", function() {
 var apiKey = "9c7c14eca6e16b6d4c30a7c992224a18";
 var cityName = $("#search").val()
 var queryURL = ("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey)
-
+//ajax call
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-        //city name
-        $("#city-title").text(response.name)
-        //date
-        var date=new Date();
-        var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
-		$("#today-date").text(val);
-        //weather icon
-        //temp
-        //humidity
-        //wind speed
-        //UV index where number has background color depending on how bad/good it is
-        console.log(cityName)
-        console.log(response.name);
-    })
+    //city name
+    $("#city-title").text(response.name)
+    //date
+    var date=new Date();
+    var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
+	$("#today-date").text(val);
+    //weather icon STILL NEED TO DO and UV index color
+    //temp
+    var main = (response.main)
+    $("#temp").text("Temperature: " + (main.temp))
+    //humidity
+    $("#humidity").text("Humidity: " + (main.humidity))
+    //wind speed
+    $("#wind-speed").text("Wind-speed: " + (response.wind.speed))
+  
+        var queryURLUV = ("http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid="+ apiKey)
+
+        $.ajax({
+        url: queryURLUV,
+        method: "GET"
+        }).then(function(data) {
+        console.log(data)
+        $("#uv-index").text("UV Index: " + data.value)
+        })
+
+  //$("#forecast").text("5 Day Forecast:")  
+
+    }) 
 
 })
