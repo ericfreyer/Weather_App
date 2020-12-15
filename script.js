a24f2a27607b27b6aa2a361e62c56bc9b7057e6b
Eric Freyer <ericcfreyer@gmail.com>
2020-12-15 11:56:32 -0500

    found long-handed way to make icon work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//do it all in one AJAX call with the second one!!!!!
 //look into using https://github.com/datejs/Datejs for dates
 $("#Search-Btn").on("click", function() {
 
@@ -11,7 +11,7 @@ $("#Search-Btn").on("click", function() {
     url: queryURL,
     method: "GET"
     }).then(function(response) {
-        //console.log(response)
+
         //city name
         $("#city-title").text(response.name);
     
@@ -22,86 +22,101 @@ $("#Search-Btn").on("click", function() {
         $("#today-date").text(val);
         
     
-        //ICONS
+
+        var queryFive = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&exclude=&appid=" + apiKey)
+        $.ajax({
+           url: queryFive,
+          method: "GET"
+        }).then(function(day){
+            console.log(day)
+           $("#forecast").show();
+           $("#footer").show();
+        
+        //ICONS NOT WORKING SOMETHING TO DO WITH "ELSE IF" If it doesn't match first criteria it will just pick the second.
         //sun
-        if((response.weather[0].id) === 800){
+        var dayOne = day.daily[0];
+        if(dayOne.weather[0].id === 800){
             $("#icon").attr("src", "assets/sun.png")
         //cloudy
-        }else if ((response.weather[0].id) === 801 || 802 || 803){
+        }else if (dayOne.weather[0].id === 801 || dayOne.weather[0].id === 802 || dayOne.weather[0].id === 803){
             $("#icon").attr("src", "assets/cloudy.png")
         //cloud
-        }else if ((response.weather[0].id) === 804){
+        }else if (dayOne.weather[0].id === 804){
             $("#icon").attr("src", "assets/cloud.png")
         //snowing
-        }else if ((response.weather[0].id) === 600 || 601 || 602 || 611 || 612 || 613 || 615 || 616 || 620 || 621 || 622){
+        }else if (dayOne.weather[0].id === 600 || dayOne.weather[0].id === 601 || dayOne.weather[0].id === 602 || dayOne.weather[0].id === 611 || dayOne.weather[0].id === 612 || dayOne.weather[0].id === 613 || dayOne.weather[0].id === 615 || dayOne.weather[0].id === 616 || dayOne.weather[0].id === 620 || dayOne.weather[0].id === 621 || dayOne.weather[0].id === 622){
             $("#icon").attr("src", "assets/snowing.png")
         //rain
-        }else if ((response.weather[0].id) === 300 || 301 || 302 || 310 || 311 || 312 || 313 || 314 || 321 || 500 || 501 || 502 || 503 || 504 || 511 || 520 || 521 || 522 || 531){
+        }else if (dayOne.weather[0].id === 300 || dayOne.weather[0].id === 301 || dayOne.weather[0].id === 302 || dayOne.weather[0].id === 310 || dayOne.weather[0].id === 311 || dayOne.weather[0].id === 312 || dayOne.weather[0].id === 313 || dayOne.weather[0].id === 314 || dayOne.weather[0].id === 321 || dayOne.weather[0].id === 500 || dayOne.weather[0].id === 501 || dayOne.weather[0].id === 502 || dayOne.weather[0].id === 503 || dayOne.weather[0].id === 504 || dayOne.weather[0].id === 511 || dayOne.weather[0].id === 520 || dayOne.weather[0].id === 521 || dayOne.weather[0].id === 522 || dayOne.weather[0].id === 531){
             $("#icon").attr("src", "assets/rain.png")
         //wind
-        }else if ((response.wind.speed) > 24){
-            $("#icon").attr("src", "assets/wind.png")
         //thunderstorm
-        }else if ((response.weather[0].id) === 200 || 201 || 202 || 210 || 211 || 212 || 221 || 230 || 231 || 232){
+        }else if (dayOne.weather[0].id === 200 || dayOne.weather[0].id === 201 || dayOne.weather[0].id === 202 || dayOne.weather[0].id === 210 || dayOne.weather[0].id === 211 || dayOne.weather[0].id === 212 || dayOne.weather[0].id === 221 || dayOne.weather[0].id === 230 || dayOne.weather[0].id === 231 || dayOne.weather[0].id === 232){
             $("#icon").attr("src", "assets/storm.png")
-        }else if ((response.weather[0].id) === 701 || 711 || 721 || 741){
-            $("#icon").attr("src", "assets/haze.png")
-        }
+        }else if(dayOne.weather[0].id === 701 || dayOne.weather[0].id === 711 || dayOne.weather[0].id === 721 || dayOne.weather[0].id === 741){
+            $("#icon").attr("src", "assets/haze.png")}
+
         
-        
+
+
         //temp
-        var fahrenheit = (((((response.main.temp) - 273.15) * 9) / 5) + 32).toFixed(2);
+        var fahrenheit = (((((dayOne.feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
         $("#temp").text("Temperature: " + (fahrenheit) + "°F");
         //humidity
-        $("#humidity").text("Humidity: " + (response.main.humidity) + "%");
+        $("#humidity").text("Humidity: " + (dayOne.humidity) + "%");
         //wind speed
-        $("#wind-speed").text("Wind-speed: " + (response.wind.speed) + " MPH");
-    
-            //AJAX for UV index
-            var queryUV = ("http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid="+ apiKey);
-    
-            $.ajax({
-            url: queryUV,
-            method: "GET"
-            }).then(function(data) {
-    
-            $("#uv-index").text("UV Index: " + data.value);
-            if(data.value < 2){
-                $("#uv-index").attr("class", "uv-good")
-            }else if(data.value > 2 < 8){
-                $("#uv-index").attr("class", "uv-moderate")
-            }else $("#uv-index").attr("class", "uv-bad")
-            })
+        $("#wind-speed").text("Wind-speed: " + (dayOne.wind_speed) + " MPH");
+
+        $("#uv-index").text("UV Index: " + dayOne.uvi);
+        if(dayOne.uvi < 2){
+            $("#uv-index").attr("class", "uv-good")
+        }else if(dayOne.uvi > 2 < 8){
+            $("#uv-index").attr("class", "uv-moderate")
+        }else $("#uv-index").attr("class", "uv-bad")
             
+        })   
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
            //this is the one to use for the 5 - day
-     var queryFive = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&exclude=&appid=" + apiKey)
-             $.ajax({
-                url: queryFive,
-               method: "GET"
-             }).then(function(day){
-                $("#forecast").show();
-                $("#footer").show();
-                     console.log(day)
-                    var fahrenheitOne = (((((day.daily[1].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
-                    var fahrenheitTwo = (((((day.daily[2].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
-                    var fahrenheitThree = (((((day.daily[3].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
-                    var fahrenheitFour = (((((day.daily[4].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
-                    var fahrenheitFive = (((((day.daily[5].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
+    //  var queryFive = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&exclude=&appid=" + apiKey)
+    //          $.ajax({
+    //             url: queryFive,
+    //            method: "GET"
+    //          }).then(function(day){
+    //             $("#forecast").show();
+    //             $("#footer").show();
+    //                  //console.log(day)
+    //                 var fahrenheitOne = (((((day.daily[1].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
+    //                 var fahrenheitTwo = (((((day.daily[2].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
+    //                 var fahrenheitThree = (((((day.daily[3].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
+    //                 var fahrenheitFour = (((((day.daily[4].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
+    //                 var fahrenheitFive = (((((day.daily[5].feels_like.day) - 273.15) * 9) / 5) + 32).toFixed(2);
 
-                    $("#day1-temp").text("Temp: " + fahrenheitOne + " °F")
-                    $("#day2-temp").text("Temp: " + fahrenheitTwo + " °F")
-                    $("#day3-temp").text("Temp: " + fahrenheitThree + " °F")
-                    $("#day4-temp").text("Temp: " + fahrenheitFour + " °F")
-                    $("#day5-temp").text("Temp: " + fahrenheitFive + " °F")
+    //                 $("#day1-temp").text("Temp: " + fahrenheitOne + " °F")
+    //                 $("#day2-temp").text("Temp: " + fahrenheitTwo + " °F")
+    //                 $("#day3-temp").text("Temp: " + fahrenheitThree + " °F")
+    //                 $("#day4-temp").text("Temp: " + fahrenheitFour + " °F")
+    //                 $("#day5-temp").text("Temp: " + fahrenheitFive + " °F")
 
-                    $("#day1-humidity").text("Humidity: " + day.daily[1].humidity + "%")
-                    $("#day2-humidity").text("Humidity: " + day.daily[2].humidity + "%")
-                    $("#day3-humidity").text("Humidity: " + day.daily[3].humidity + "%")
-                    $("#day4-humidity").text("Humidity: " + day.daily[4].humidity + "%")
-                    $("#day5-humidity").text("Humidity: " + day.daily[5].humidity + "%")
-             })
+    //                 $("#day1-humidity").text("Humidity: " + day.daily[1].humidity + "%")
+    //                 $("#day2-humidity").text("Humidity: " + day.daily[2].humidity + "%")
+    //                 $("#day3-humidity").text("Humidity: " + day.daily[3].humidity + "%")
+    //                 $("#day4-humidity").text("Humidity: " + day.daily[4].humidity + "%")
+    //                 $("#day5-humidity").text("Humidity: " + day.daily[5].humidity + "%")
+    //          })
     
-        })
+        
     
             //press ctrl + / to uncomment
             //AJAX for five day forecast MIGHT WANT TO USE DIFFERENT API TO GET THE ICONS FOR THER FIVE DAY> NEED A GENERAL ID FOR THE WHOLE DAY
@@ -239,4 +254,4 @@ $("#Search-Btn").on("click", function() {
     
     
     
-    })
+    
